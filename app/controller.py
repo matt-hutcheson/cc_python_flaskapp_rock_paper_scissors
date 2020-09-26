6eff9b7f6c1135a0805b62643f208998c0c1ea39
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-26 11:36:49 +0100

    connected html to controller

--- app/controller.py
+++ app/controller.py
@@ -1,5 +1,10 @@
 from app import app
 
+from flask import render_template
+
+from app.models.src.player import *
+from app.models.src.game import *
+
 @app.route('/')
 def index():
-    return "ok"
+    return render_template("base.html", title="Home")
