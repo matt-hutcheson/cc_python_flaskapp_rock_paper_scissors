dc171ec95060da7da9504e723c6de3ac0f11d2f2
matt-hutcheson <matt.hutcheson@blueyonder.co.uk>
2020-09-26 10:57:15 +0100

    added tests p1 rock p2 paper/scissors and passed

--- app/models/tests/game_test.py
+++ app/models/tests/game_test.py
@@ -8,8 +8,20 @@ class TestGame(unittest.TestCase):
         # self.player_1_name = "Bob"
         # self.player_2_name = "Tom"
 
-    def test_draw(self):
+    def test_draw__None(self):
         test_player_1 = Player("Bob", "Rock")
         test_player_2 = Player("Tom", "Rock")
         test_game = Game(test_player_1, test_player_2)
         self.assertEqual(None, test_game.play_game(test_game))
+
+    def test_rock_paper__player2wins(self):
+        test_player_1 = Player("Bob", "Rock")
+        test_player_2 = Player("Tom", "Paper")
+        test_game = Game(test_player_1, test_player_2)
+        self.assertEqual("Tom wins!", test_game.play_game(test_game))
+
+    def test_rock_scissors__player1wins(self):
+        test_player_1 = Player("Bob", "Rock")
+        test_player_2 = Player("Tom", "Scissors")
+        test_game = Game(test_player_1, test_player_2)
+        self.assertEqual("Bob wins!", test_game.play_game(test_game))
